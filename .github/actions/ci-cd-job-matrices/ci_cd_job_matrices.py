bcbece1855c2689e081212def879ee62f9790ce4
vgelbgras <vgelbgra@synopsys.com>
2026-09-22 13:48:02 +0200

    fix pre-commit

--- .github/actions/ci-cd-job-matrices/ci_cd_job_matrices.py
+++ .github/actions/ci-cd-job-matrices/ci_cd_job_matrices.py
@@ -115,7 +115,9 @@ def get_pr_changes() -> list[str]:
 
 
 def get_changed_moon_packages(pr_changes: list[str]) -> list[str]:
-    changed_packages = [pkg for pkg in pr_changes if pkg in SAF_PACKAGES and pkg in UV_PACKAGES]
+    changed_packages = [
+        pkg for pkg in pr_changes if pkg in SAF_PACKAGES and pkg in UV_PACKAGES
+    ]
     write_matrix_to_output(
         "moon_packages_matrix", [{"library-name": pkg} for pkg in changed_packages]
     )
@@ -123,12 +125,13 @@ def get_changed_moon_packages(pr_changes: list[str]) -> list[str]:
 
 
 def get_changed_poetry_packages(pr_changes: list[str]) -> list[str]:
-    changed_packages = [pkg for pkg in pr_changes if pkg in SAF_PACKAGES and pkg not in UV_PACKAGES]
+    changed_packages = [
+        pkg for pkg in pr_changes if pkg in SAF_PACKAGES and pkg not in UV_PACKAGES
+    ]
     write_matrix_to_output(
         "poetry_packages_matrix", [{"library-name": pkg} for pkg in changed_packages]
     )
     return changed_packages
-
 
 
 def get_changed_packages(pr_changes: list[str]) -> list[str]:
