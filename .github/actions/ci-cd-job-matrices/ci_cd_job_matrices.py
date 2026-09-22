d9220d05f03c7682316f875830067c349268fd38
vgelbgras <vgelbgra@synopsys.com>
2026-09-22 13:57:42 +0200

    adjust matrix

--- .github/actions/ci-cd-job-matrices/ci_cd_job_matrices.py
+++ .github/actions/ci-cd-job-matrices/ci_cd_job_matrices.py
@@ -116,7 +116,7 @@ def get_pr_changes() -> list[str]:
 
 def get_changed_moon_packages(pr_changes: list[str]) -> list[str]:
     changed_packages = [
-        pkg for pkg in pr_changes if pkg in SAF_PACKAGES and pkg in UV_PACKAGES
+        pkg for pkg in pr_changes if pkg in UV_PACKAGES
     ]
     write_matrix_to_output(
         "moon_packages_matrix", [{"library-name": pkg} for pkg in changed_packages]
